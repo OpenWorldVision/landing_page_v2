e22698a1449adb3583678901fccfe05098d74c8d
dang.duonghai <duonghaidang2k@gmail.com>
2023-05-18 22:29:16 +0700

    feat: set meta tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,7 +7,35 @@ export default function Document() {
         href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&family=Roboto&display=swap"
         rel="stylesheet"
       />
-      <Head />
+      <Head>
+        <meta name="identifier-url" content="https://openworld.vision/" />
+        <meta name="title" content="Openworld #DeFi, #DEX" />
+        <meta
+          name="description"
+          content="OpenWorld is a decentralize derivative exchange"
+        />
+        <meta
+          name="abstract"
+          content="OpenWorld is a decentralize derivative exchange"
+        />
+        <meta name="keywords" content="ow, openworld, defi, dex" />
+        <meta name="author" content="OpenWorld" />
+        <meta name="revisit-after" content="15" />
+        <meta name="language" content="EN" />
+        <meta
+          name="copyright"
+          content="Copyright© 2021-2023. All Rights Reserved"
+        />
+        <meta name="robots" content="All" />
+        <meta property="og:image" content="/wp/wp-content/uploads/2022/12/share-image.png"/>
+        <meta property="og:image:width" content="1200"/>
+        <meta property="og:image:height" content="630"/>
+        <meta property="og:image:type" content="image/png"/>
+        <meta name="twitter:card" content="summary_large_image"/>
+        <meta name="twitter:title" content="Openworld #DeFi, #DEX"/>
+        <meta name="twitter:description" content="OpenWorld is a decentralize derivative exchange"/>
+        <meta name="twitter:image" content="/wp/wp-content/uploads/2022/12/share-image.png"/>
+      </Head>
       <body>
         <Main />
         <NextScript />
