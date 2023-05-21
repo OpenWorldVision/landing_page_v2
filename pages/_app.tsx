d84c9b6e1b329d0ac0ae8102ef4b171e908b3b16
Phuong Nguyen <nnphuong1997@gmail.com>
2023-05-21 23:36:08 +0700

    optimize font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,21 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
+import { Montserrat } from "next/font/google";
+const montserrat = Montserrat({ subsets: ["latin"] });
+
 config.autoAddCss = false;
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${montserrat.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  );
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,10 +4,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&family=Roboto&display=swap"
-          rel="stylesheet"
-        />
         <meta name="identifier-url" content="https://openworld.vision/" />
         <meta name="title" content="Openworld #DeFi, #DEX" />
         <meta
